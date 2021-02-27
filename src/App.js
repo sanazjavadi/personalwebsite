2aab85f4b5f4fabdf3a533311f55e6e1bd9e6fbb
sanaz_javadi <sanazjavadi72@yahoo.com>
2021-02-27 19:02:26 +0330

    redesign

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
 
   const [ligth, setLight] = useState(true)
   const [open, setOpen] = useState(true)
+  const [clicked, setClicked]= useState(false)
   useEffect(() => eyeLead())
+
   const eyeLead = () => {
     const wrapper = document.querySelector('.wrapper')
     wrapper.addEventListener('mousemove', (e) => {
@@ -28,110 +30,116 @@ function App() {
   }
   return (
 
-      <div
-        className="wrapper"
-        style=
-        {ligth ? {
-          backgroundColor: '#2f2c43'
-        } : { backgroundColor: '#2f2c43' }}
+    <div
+      className="wrapper"
+      style=
+      {ligth ? {
+        backgroundColor: '#2f2c43'
+      } : { backgroundColor: '#2f2c43' }}
 
-      >
-        
-        <div className="vector" onClick={() =>{
-           setLight(!ligth)
-           setOpen(!open)
-        }}>
-          <div className="light">
-            {
-              ligth && <div className="light-on"> </div>
-            }
+    >
+      
 
-            <div className="bolb"> </div>
-          </div>
-          <div className={`laptop ${!open ? 'laptop--closed' : ''}`}>
-    <div className="laptop-block"></div>
-    <div className="laptop-top">
-      <div></div>
-      <div></div>
-    </div>
-    <div className="laptop-bottom">
-      <div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+      <div className="vector" onClick={() => {
+        setLight(!ligth)
+        setOpen(!open)
+      }}>
+
+        <div className="light">
+          {
+            ligth && <div className="light-on"> </div>
+          }
+
+          <div className="bolb"> </div>
         </div>
-        <div></div>
-      </div>
-      <div></div>
-    </div>
-  </div>
-
-          {/* <div className="desktop">
-            
-          <div class="content">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li style={{width: '90%'}}></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li style={{width: '60%'}}></li>
-            <li></li>
-            <li></li>
-            <li style={{width: '70%'}}></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li style={{width: '50%'}}></li>
-          </ul>
-        </div>
-             </div> */}
-          <div className="desk"></div>
+        <div className="workspace">
           <div className="coffee">
             <div className="eyes">
               <div className="eye"></div>
               <div className="eye eye2"></div>
             </div>
           </div>
-        </div>
-
-        <div className="description">
-          <p>
-            Hi,I'm sanaz I like making fun,interactive things with code.
-      </p>
-          <div className="social">
-            <span>
-
-              <Github width="30" height="30"/>
-
-            </span>
-            <span>
-
-              <Linkdin width="30" height="30"/>
-
-            </span>
-            <span>
-
-              <Codepen width="30" height="30"/>
-
-            </span>
-            <span>
-
-              <Twitter width="30" height="30"/>
-
-            </span>
+          <div className={`laptop ${!open ? 'laptop--closed' : ''}`}>
+            
+            <div className="laptop-block">
+             
+            </div>
+            <div className="laptop-top">
+              
+              <div>
+                
+              </div>
+              <div>
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li style={{width:'50%'}}></li>
+                  <li></li>
+                  <li style={{width:'70%'}}></li>
+                  <li></li>
+                  <li></li>
+                  <li style={{width:'50%'}}></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+               </div>
+            </div>
+            <div className="laptop-bottom">
+              <div>
+                <div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div></div>
+              </div>
+              <div></div>
+            </div>
           </div>
         </div>
-      </div >
-  
+
+
+
+        <div className="desk"></div>
+
+      </div>
+      <button class={`toggle ${clicked && 'clicked'}`} id="toggle" onClick={()=> {
+        setClicked(!clicked)
+        setOpen(!open)
+        setLight(!ligth)
+      }}></button>
+      <div className="description">
+     
+        <p>
+          Hi,I'm sanaz I like making fun,interactive things with code.
+      </p>
+        <div className="social">
+          <span>
+
+            <Github width="30" height="30" />
+
+          </span>
+          <span>
+
+            <Linkdin width="30" height="30" />
+
+          </span>
+          <span>
+
+            <Codepen width="30" height="30" />
+
+          </span>
+          <span>
+
+            <Twitter width="30" height="30" />
+
+          </span>
+        </div>
+      
+      </div>
+     
+    </div >
+
   )
 }
 
